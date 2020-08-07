51376a031e81166df46cbe54bc3a6823ea949f65
amrka <amr.kamel@uv.es>
2020-08-07 11:14:09 +0200

    change the name of the workflow

--- resting_state_correlation_analysis_0007.py
+++ resting_state_correlation_analysis_0007.py
@@ -48,8 +48,8 @@ datasink.inputs.container = output_dir
 datasink.inputs.base_directory = experiment_dir
 
 substitutions = [('_map_id_', ''),
-('_contrast_..home..in..aeed..Work..October_Acquistion..VBM..VBM_corr_designs..', ''),
-('design_..home..in..aeed..Work..October_Acquistion..VBM..VBM_corr_designs..', ''),
+('_contrast_..home..in..aeed..Work..October_Acquistion..VBM..resting_state_corr_designs..', ''),
+('design_..home..in..aeed..Work..October_Acquistion..VBM..resting_state_corr_designs..', ''),
 ]
 
 datasink.inputs.substitutions = substitutions
@@ -204,7 +204,7 @@ create_corr2.inputs.out_file = 'corr_coef_r2.nii.gz'
 
 
 #-----------------------------------------------------------------------------------------------------
-VBM_corr.connect ([
+resting_state_corr.connect ([
 
       (infosource, selectfiles,[('map_id','map_id')]),
 
@@ -249,7 +249,7 @@ VBM_corr.connect ([
   ])
 
 
-VBM_corr.write_graph(graph2use='colored', format='svg', simple_form=True)
-# VBM_corr.run(plugin='SLURM', plugin_args={'dont_resubmit_completed_jobs': True,'max_jobs':50, 'sbatch_args':'--mem=16G'})
+resting_state_corr.write_graph(graph2use='colored', format='svg', simple_form=True)
+# resting_state_corr.run(plugin='SLURM', plugin_args={'dont_resubmit_completed_jobs': True,'max_jobs':50, 'sbatch_args':'--mem=16G'})
 # plugin_args={'sbatch_args': '--time=24:00:00 -N1 -c2 --mem=40G','max_jobs':200}
-VBM_corr.run('MultiProc', plugin_args={'n_procs': 8})
+resting_state_corr.run('MultiProc', plugin_args={'n_procs': 8})
