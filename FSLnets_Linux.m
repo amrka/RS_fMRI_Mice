5a04e704beb7629d540bd53ec68b3c316ff748c3
Amr <amr.kamel@uv.es>
2020-09-14 10:42:17 +0200

    uncomment several fields

--- FSLnets_Linux.m
+++ FSLnets_Linux.m
@@ -49,8 +49,10 @@ ts_spectra=nets_spectra(ts);             % have a look at mean spectra after thi
 %netmats0a= nets_netmats(ts,0,'amp');        % amplitudes only - no correlations (just the diagonal)
 netmats_F=  nets_netmats(ts,1,'corr');       % full correlation (normalised covariances)
 netmats_P=  nets_netmats(ts,1,'icov');       % partial correlation
+netmats_rP=  nets_netmats(ts,1,'ridgep', 0.1);     % Ridge Regression partial, with rho=0.1
+
 % netmats3=  nets_netmats(ts,1,'icov',10);    % L1-regularised partial, with lambda=10
-% netmats_rP=  nets_netmats(ts,1,'ridgep', 0.1);     % Ridge Regression partial, with rho=0.1
+
 
 %netmats11= nets_netmats(ts,0,'pwling');     % Hyvarinen's pairwise causality measure
 %%
@@ -58,17 +60,21 @@ netmats_P=  nets_netmats(ts,1,'icov');       % partial correlation
 
 save('/media/amr/Amr_4TB/Work/October_Acquistion/resting_state/resting_state_gp_ICA+DR_workingdir/melodic_workflow/_dim_20/melodic_group/dim_20_netmats_F.mat', 'netmats_F')
 save('/media/amr/Amr_4TB/Work/October_Acquistion/resting_state/resting_state_gp_ICA+DR_workingdir/melodic_workflow/_dim_20/melodic_group/dim_20_netmats_P.mat', 'netmats_P')
-% save('/media/amr/Amr_4TB/Work/October_Acquistion/resting_state/resting_state_gp_ICA+DR_workingdir/melodic_workflow/_dim_20/melodic_group/dim_20_netmats3.mat', 'netmats3')
 save('/media/amr/Amr_4TB/Work/October_Acquistion/resting_state/resting_state_gp_ICA+DR_workingdir/melodic_workflow/_dim_20/melodic_group/dim_20_netmats_rP.mat', 'netmats_rP')
+
+
+% save('/media/amr/Amr_4TB/Work/October_Acquistion/resting_state/resting_state_gp_ICA+DR_workingdir/melodic_workflow/_dim_20/melodic_group/dim_20_netmats3.mat', 'netmats3')
 %%
 %%% view of consistency of netmats across subjects; returns t-test Z values as a network matrix
 %%% second argument (0 or 1) determines whether to display the Z matrix and a consistency scatter plot
 %%% third argument (optional) groups runs together; e.g. setting this to 4 means each group of 4 runs were from the same subject
 [Znet_F,Mnet_F]=nets_groupmean(netmats_F,1);   % test whichever netmat you're interested in; returns Z values from one-group t-test and group-mean netmat
 [Znet_P,Mnet_P]=nets_groupmean(netmats_P,1);   % test whichever netmat you're interested in; returns Z values from one-group t-test and group-mean netmat
+[Znet_rP,Mnet_rP]=nets_groupmean(netmats_rP,1);   % test whichever netmat you're interested in; returns Z values from one-group t-test and group-mean netmat
+
+
 % [Znet3,Mnet3]=nets_groupmean(netmats3,1);   % test whichever netmat you're interested in; returns Z values from one-group t-test and group-mean netmat
 
-% [Znet_rP,Mnet_rP]=nets_groupmean(netmats_rP,1);   % test whichever netmat you're interested in; returns Z values from one-group t-test and group-mean netmat
 
 %%
 %%% view hierarchical clustering of nodes
@@ -77,12 +83,15 @@ save('/media/amr/Amr_4TB/Work/October_Acquistion/resting_state/resting_state_gp_
 % obivously in Mac's more recent version, the nets_hierarchy.m script was changed and requires 3 slices images
 % the problem was resolved once I replaced that version of nets_hierarchy with the linux one (the other one is renamed _net_hierarchy.m )
 nets_hierarchy(Znet_F,Znet_P,ts.DD,'/media/amr/Amr_4TB/Work/October_Acquistion/resting_state/resting_state_gp_ICA+DR_workingdir/melodic_workflow/_dim_20/melodic_group/melodic_IC');
-%nets_hierarchy(Znet_F,Znet_rP,ts.DD,'/media/amr/Amr_4TB/Work/October_Acquistion/resting_state/resting_state_gp_ICA+DR_workingdir/melodic_workflow/_dim_20/melodic_group/melodic_IC');
+nets_hierarchy(Znet_F,Znet_rP,ts.DD,'/media/amr/Amr_4TB/Work/October_Acquistion/resting_state/resting_state_gp_ICA+DR_workingdir/melodic_workflow/_dim_20/melodic_group/melodic_IC');
+nets_hierarchy(Znet_P,Znet_rP,ts.DD,'/media/amr/Amr_4TB/Work/October_Acquistion/resting_state/resting_state_gp_ICA+DR_workingdir/melodic_workflow/_dim_20/melodic_group/melodic_IC');
+
 
 %%% view interactive netmat web-based display
 % for this to work, you need to install XAMPP, and save the index.html in /Applications/XAMPP/htdocs
 % it goes without saying, but you need to start the Apache Web Server
-% nets_netweb(Znet_F,Znet_P,ts.DD,'/media/amr/Amr_4TB/Work/October_Acquistion/resting_state/resting_state_gp_ICA+DR_workingdir/melodic_workflow/_dim_20/melodic_group/melodic_IC','/opt/lampp/htdocs');
+nets_netweb(Znet_F,Znet_P,ts.DD,'/media/amr/Amr_4TB/Work/October_Acquistion/resting_state/resting_state_gp_ICA+DR_workingdir/melodic_workflow/_dim_20/melodic_group/melodic_IC','/opt/lampp/htdocs/netweb_F_P');
+nets_netweb(Znet_F,Znet_rP,ts.DD,'/media/amr/Amr_4TB/Work/October_Acquistion/resting_state/resting_state_gp_ICA+DR_workingdir/melodic_workflow/_dim_20/melodic_group/melodic_IC','/opt/lampp/htdocs/netweb_F_rP');
 
 
 %%% cross-subject GLM, with inference in randomise (assuming you already have the GLM design.mat and design.con files).
@@ -93,9 +102,11 @@ contrast = '/media/amr/Amr_4TB/Work/October_Acquistion/resting_state/Design_octo
 % I adjusted the number of permutations to 10000 from nets_glm.m
 [p_uncorrected_F,p_corrected_F]=nets_glm(netmats_F, design, contrast,1); %1 last argument is to show output or not
 [p_uncorrected_P,p_corrected_P]=nets_glm(netmats_P, design, contrast,1);
+[p_uncorrected_rP,p_corrected_rP]=nets_glm(netmats_rP, design, contrast,1);
+
 % [p_uncorrected3,p_corrected3]=nets_glm(netmats3, design, contrast,1);
 
-[p_uncorrected_rP,p_corrected_rP]=nets_glm(netmats_rP, design, contrast,1);
+
 % returns matrices of 1-p
 %%% OR - GLM, but with pre-masking that tests only the connections that are strong on average across all subjects.
 %%% change the "8" to a different tstat threshold to make this sparser or less sparse.
@@ -105,13 +116,17 @@ contrast = '/media/amr/Amr_4TB/Work/October_Acquistion/resting_state/Design_octo
 %%% view 6 most significant edges from this GLM
 nets_edgepics(ts,group_maps,Znet_F,reshape(p_corrected_F(1,:),ts.Nnodes,ts.Nnodes),6); %
 nets_edgepics(ts,group_maps,Znet_P,reshape(p_corrected_P(1,:),ts.Nnodes,ts.Nnodes),6);
-% nets_edgepics(ts,group_maps,Znet3,reshape(p_corrected3(1,:),ts.Nnodes,ts.Nnodes),6);
 nets_edgepics(ts,group_maps,Znet_rP,reshape(p_corrected_rP(1,:),ts.Nnodes,ts.Nnodes),6);
 
-% nets_edgepics(ts,group_maps,Znet_F,reshape(p_corrected_F(2,:),ts.Nnodes,ts.Nnodes),6); %
-% nets_edgepics(ts,group_maps,Znet_P,reshape(p_corrected_P(2,:),ts.Nnodes,ts.Nnodes),6);
+% nets_edgepics(ts,group_maps,Znet3,reshape(p_corrected3(1,:),ts.Nnodes,ts.Nnodes),6);
+
+
+nets_edgepics(ts,group_maps,Znet_F,reshape(p_corrected_F(2,:),ts.Nnodes,ts.Nnodes),6); %
+nets_edgepics(ts,group_maps,Znet_P,reshape(p_corrected_P(2,:),ts.Nnodes,ts.Nnodes),6);
+nets_edgepics(ts,group_maps,Znet_rP,reshape(p_corrected_rP(2,:),ts.Nnodes,ts.Nnodes),6);
+
 % nets_edgepics(ts,group_maps,Znet3,reshape(p_corrected3(2,:),ts.Nnodes,ts.Nnodes),6);
-% nets_edgepics(ts,group_maps,Znet5,reshape(p_corrected5(2,:),ts.Nnodes,ts.Nnodes),6);
+
 %%
 %%% simple cross-subject multivariate discriminant analyses, for just two-group cases.
 %%% arg1 is whichever netmats you want to test.
@@ -119,9 +134,11 @@ nets_edgepics(ts,group_maps,Znet_rP,reshape(p_corrected_rP(1,:),ts.Nnodes,ts.Nno
 %%% arg3 determines which LDA method to use (help nets_lda to see list of options)
 [lda_percentages]=nets_lda(netmats_F,14,7)
 [lda_percentages]=nets_lda(netmats_P,14,7)
+[lda_percentages]=nets_lda(netmats_rP,14,7)
+
 % [lda_percentages]=nets_lda(netmats3,14,7)
 
-[lda_percentages]=nets_lda(netmats_rP,14,7)
+
 
 %%% create boxplots for the two groups for a network-matrix-element of interest (e.g., selected from GLM output)
 %%% arg3 = matrix row number,    i.e. the first  component of interest (from the DD list)
