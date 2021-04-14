bd30f1b9b5a22970bb85558da13cbb216c1cb717
amrka <amr.kamel@uv.es>
2021-04-14 13:10:09 +0200

    fix the FSL directory

--- FC_behavior_correlations_Mac.m
+++ FC_behavior_correlations_Mac.m
@@ -1,5 +1,5 @@
 % Set up FSL environment
-setenv( 'FSLDIR', '/usr/share/fsl');
+setenv( 'FSLDIR', '/usr/local/fsl');
 fsldir = getenv('FSLDIR');
 fsldirmpath = sprintf('%s/etc/matlab',fsldir);
 path(path, fsldirmpath);
