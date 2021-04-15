e6f6f1f37cde626ceeaafc3fbb67bb6b0941f396
amrka <amr.kamel@uv.es>
2021-04-15 18:12:29 +0200

    get the behavioral values

--- plot_amplitude_behavior_correlation.py
+++ plot_amplitude_behavior_correlation.py
@@ -19,7 +19,7 @@ import os
 from scipy.io import loadmat
 
 
-def plot_amplitude_correlation(amp_mat_path, netmats_A_path):
+def plot_amplitude_correlation(amp_mat_path, netmats_A_path, design_mat_path):
     # 1- get the amplitudes
     # load the matlab mat to python
     mat = loadmat(amp_mat_path)
@@ -27,6 +27,8 @@ def plot_amplitude_correlation(amp_mat_path, netmats_A_path):
     mat_ndarray = mat['p_corrected_A']
     # get the index of the significant element in the matrix
     loc = np.nonzero(mat_ndarray > 0.949)
+    # get the exact p_values to use it later
+    p_value = mat_ndarray[mat_ndarray > 0.949]
     # convert the ndarray containing the column location to a list of one element
     index_sig_col = loc[1].astype(list)
     # load the netmats_A matrix
@@ -34,3 +36,21 @@ def plot_amplitude_correlation(amp_mat_path, netmats_A_path):
     netmats_A = netmats['netmats_A']
     # use the col index to get the amplitudes of the 30 subjects from the netmats
     amplitudes = netmats_A[:, index_sig_col]
+
+    # 2- get the behavioral values from design matrix
+    mat_basename_no_ext = ntpath.basename(design_mat_path)[:-4]
+    fh = open(design_mat_path, 'r')
+    lines = fh.readlines()
+    fh.close()
+    behav = []
+    i = 5
+    for item in lines:
+        while i < len(lines):
+            behav.append(float(lines[i][13:-2]))
+            i = i + 1
+
+    # sanity check
+    print("length of amplitudes_values -> {0}".format(len(amplitudes)))
+    print("length of behav_values -> {0}".format(len(behav)))
+    if len(amplitudes) != len(behav):
+        sys.error('######ERROR####')
